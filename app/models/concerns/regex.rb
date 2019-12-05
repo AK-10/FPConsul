38c4206bdd5e3ab39e16b2528faccebb0890eb9b
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-05 15:45:24 +0900

    email_format: change class method to constant

--- app/models/concerns/regex.rb
+++ app/models/concerns/regex.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 module Regex
-  extend ActiveSupport::Concern
 
-  def self.email_format
-    %r{\A[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z}
-  end
+  EmailFormat = %r{\A[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z}
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   include Regex
 
   has_secure_password
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, uniqueness: true, length: { maximum: 100 }, format: { with: Regex.email_format }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 100 }, format: { with: Regex::EmailFormat }
 end
