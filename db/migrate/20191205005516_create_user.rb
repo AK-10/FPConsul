ab9987ac418036e9bd4db220388a91f29f7f83a1
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-05 12:30:55 +0900

    fix maximum limit constraint for user.email, 320 -> 100

--- db/migrate/20191205005516_create_user.rb
+++ db/migrate/20191205005516_create_user.rb
@@ -2,11 +2,12 @@ class CreateUser < ActiveRecord::Migration[5.2]
   def up
     create_table :users do |t|
       t.string :name, null: false, limit: 50
-      t.string :email, null: false, limit: 320, unique: true
+      t.string :email, null: false, limit: 100, unique: true
       t.string :password_digest, null: false 
       t.timestamps
     end
   end
+  
   def down
     drop_table :users
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2019_12_05_005516) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", limit: 50, null: false
-    t.string "email", limit: 319, null: false
+    t.string "email", limit: 100, null: false
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
