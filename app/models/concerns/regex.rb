765910c0bffde2be5e05d9ed119933a88d1663a3
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-05 14:15:18 +0900

    rubocop

--- app/models/concerns/regex.rb
+++ app/models/concerns/regex.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 module Regex
   extend ActiveSupport::Concern
 
-  def  self.email_format
-    /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/
+  def self.email_format
+    %r{\A[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z}
   end
-
 end
