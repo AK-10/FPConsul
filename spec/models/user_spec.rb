e2003719455bee3f121f45a8318b3b76ac1f04b9
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-09 11:13:50 +0900

    WIP: impl user spec with shoulda-matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,39 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
-  it 'is valid with name, email' do
-    user = build(:user)
+  describe 'validations' do
+    context 'name' do
+      it { should validate_presence_of(:name) }
+      it { should validate_length_of(:name).is_at_most(50) }
+    end
     
-    expect(user).to be_valid
-  end
-
-  it 'is invalid with blank name, blank email' do
-    user = build(:user)
-    user.name = ''
-    user.email = ''
-
-    expect(user).to be_invalid
-  end
-
-  it 'is invalid with blank name' do
-    user = build(:user)
-    user.name = ''
-
-    expect(user).to be_invalid
-  end
-
-  it 'is invalid with blank email' do
-    user = build(:user)
-    user.email = ''
-
-    expect(user).to be_invalid
-  end
-
-  it 'is invalid with unnormalized email' do
-    user = build(:user)
-    user.email = 'aaaaaaaaaaaa'
-
-    expect(user).to be_invalid
+    context 'email' do
+      it { should validate_presence_of(:email) }
+      it { should validate_length_of(:email).is_at_most(100) }
+      it { should validate_uniqueness_of(:email) }
+      
+      it { should allow_value('test@test.com').for(:email) }
+      it { should_not allow_value('abcdefg').for(:email) }
+    end
+    
+    context 'password' do
+      it { should have_secure_password }
+    end
   end
 end
