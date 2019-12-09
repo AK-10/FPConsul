a15e1b9e4ace2f4107175d91658fcbd6ef30f127
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-09 11:43:08 +0900

    fix user factory: change password value

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name { '金山　太郎' }
     email { 'test@example.com' }
-    password { 'password' }
-    password_confirmation { 'password' }
+    password { 'aBcDeFgHi' }
+    password_confirmation { 'aBcDeFgHi' }
   end
 end
