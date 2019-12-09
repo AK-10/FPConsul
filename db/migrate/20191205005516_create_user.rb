03623b39d3f33eda05d828190af486fa6331539d
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-09 14:07:38 +0900

    add index for email, add unique constraint

--- db/migrate/20191205005516_create_user.rb
+++ db/migrate/20191205005516_create_user.rb
@@ -2,9 +2,11 @@ class CreateUser < ActiveRecord::Migration[5.2]
   def up
     create_table :users do |t|
       t.string :name, null: false, limit: 50
-      t.string :email, null: false, limit: 100, unique: true
+      t.string :email, null: false, limit: 100
       t.string :password_digest, null: false 
       t.timestamps
+      
+      t.index :email, unique: true
     end
   end
   
