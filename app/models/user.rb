a3cd42808dfa5c34e0c8fd877ed2fea2338f264e
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-09 13:40:33 +0900

    rename EmailFormat -> EMAIL_FORMAT

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true, length: { maximum: 50 }, allow_nil: true
-  validates :email, presence: true, uniqueness: true, length: { maximum: 100 }, format: { with: Regex::EmailFormat }, allow_nil: true
+  validates :email, presence: true, uniqueness: true, length: { maximum: 100 }, format: { with: Regex::EMAIL_FORMAT }, allow_nil: true
 end
