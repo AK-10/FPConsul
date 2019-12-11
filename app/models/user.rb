bc9cdb05ea1f065208a8b836906cec8d8e854edd
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-11 11:26:57 +0900

    add associations for user and planner, has_many reservations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   include ValidFormat
 
   has_secure_password
+  has_many :reservations
 
   validates :name, presence: true, length: { maximum: 50 }, allow_nil: true
   validates :email, presence: true, uniqueness: true, length: { maximum: 100 }, format: { with: ValidFormat::EMAIL_FORMAT }, allow_nil: true
