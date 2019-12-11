e7c78da12b5ca98f1c3f300c757b3b3ab61c207d
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-11 12:05:35 +0900

    remove with_foreign key

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
   describe 'validations' do
-    it { is_expected.to belongs_to(:user).with_foreign_key(:user_id) }
-    it { is_expected.to belongs_to(:planner).with_foreign_key(:planner_id) }
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:planner) }
 
     it { is_expected.to validate_presence_of(:scheduled_at) }
     it { is_expected.to validate_length_of(:description).is_at_most(200) }
