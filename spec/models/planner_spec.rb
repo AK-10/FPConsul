bcdb22255e3160d28b892fb2a8e2ca1b0f72221f
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-12 11:28:38 +0900

    remove a line at planner_spec.rb

--- spec/models/planner_spec.rb
+++ spec/models/planner_spec.rb
@@ -1,28 +1,27 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Planner, type: :model do
-  describe 'validations' do
+  describe "validations" do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_length_of(:name).is_at_most(50) }
 
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_length_of(:email).is_at_most(100) }
 
-    it { is_expected.to allow_value('test@test.com').for(:email) }
-    it { is_expected.not_to allow_value('abcdefg').for(:email) }
+    it { is_expected.to allow_value("test@test.com").for(:email) }
+    it { is_expected.not_to allow_value("abcdefg").for(:email) }
 
     it { is_expected.to have_secure_password }
 
-
-    context 'email uniqueness validation' do
+    context "email uniqueness validation" do
       before { create(:planner) }
       it { is_expected.to validate_uniqueness_of(:email) }
     end
   end
 
-  describe 'associations' do
+  describe "associations" do
     it { is_expected.to have_many(:reservations) }
   end
 end
