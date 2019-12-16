cdb93f692ca9517fb34baa3a7d31345d03c2af4f
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-16 11:55:21 +0900

    difine available frames

--- db/migrate/20191216023929_create_avaliable_frame.rb
+++ db/migrate/20191216023929_create_avaliable_frame.rb
@@ -1,6 +1,15 @@
 class CreateAvaliableFrame < ActiveRecord::Migration[5.2]
-  def change
+  def up
     create_table :avaliable_frames do |t|
+      t.references :planner, foreign_key: true
+      t.datetime :date_time, comment: "予約可能な日時と開始時間"
+      t.timestamps
+
+      t.index [:planner_id, :date_time], unique: true
     end
+  end
+
+  def down
+    drop_table :avaliable_frames
   end
 end
