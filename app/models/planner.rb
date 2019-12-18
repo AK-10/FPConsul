2600b3fe646d19e088e7959dd7e0e074fbc1a5f9
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-18 17:48:53 +0900

    add reservations association for user, planner

--- app/models/planner.rb
+++ app/models/planner.rb
@@ -3,6 +3,7 @@
 class Planner < ApplicationRecord
   has_secure_password
   has_many :available_frames, dependent: :destroy
+  has_many :reservations, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 100 }, format: { with: ValidFormat::EMAIL_FORMAT }
