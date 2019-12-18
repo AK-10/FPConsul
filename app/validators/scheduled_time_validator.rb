40377b0ae0fbaaebd26d2d96f35887facb8db1ff
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-18 18:36:57 +0900

    add const for range of time

--- app/validators/scheduled_time_validator.rb
+++ app/validators/scheduled_time_validator.rb
@@ -1,6 +1,11 @@
 class ScheduledTimeValidator < ActiveModel::EachValidator
   attr_reader :record, :attribute, :value
   
+  START_TIME_ON_WEEKDAYS = { hour: 10, min: 0 }
+  END_TIME_ON_WEEKDAYS = { hour: 17, min: 30 }
+  START_TIME_ON_SATURDAY = { hour: 11, min: 0 }
+  END_TIME_ON_SATURDAY = { hour: 14, min: 30 }
+
   def validate_each(record, attribute, value)
     @record = record
     @attribute = attribute
@@ -16,16 +21,16 @@ class ScheduledTimeValidator < ActiveModel::EachValidator
   private
 
     def validate_time_range
-      start_time = value.change(hour: 10, min: 0)
-      end_time = value.change(hour: 17, min: 30)
+      start_time = value.change(START_TIME_ON_WEEKDAYS)
+      end_time = value.change(END_TIME_ON_WEEKDAYS)
       return if value.between?(start_time, end_time)
 
       record.errors.add(attribute, "can't be except 10:00 - 18:00")
     end
 
     def validate_time_range_on_saturday
-      start_time = value.change(hour: 11, min: 0)
-      end_time = value.change(hour: 14, min: 30)
+      start_time = value.change(START_TIME_ON_SATURDAY)
+      end_time = value.change(END_TIME_ON_SATURDAY)
 
       return unless value.saturday?
       return if value.between?(start_time, end_time)
