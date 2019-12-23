562f023d1663b3c20c8550d94cd8b1549f78f2ba
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-23 15:33:48 +0900

    be rails g controller User new edit

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,10 @@ module FPConsul
 
     config.active_record.schema_format = :sql
 
+    config.generators do |gen|
+      gen.assets false
+      gen.helper false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'user/new'
+  get 'user/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
