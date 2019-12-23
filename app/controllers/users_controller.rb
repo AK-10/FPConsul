f2504941acdd002b60aeb645b6928fecc3c02807
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-23 17:13:49 +0900

    impl create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,34 @@ class UsersController < ApplicationController
   def new
   end
 
+  def show
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'ユーザー登録が完了しました.'
+      redirect_to @user
+    else
+      flash[:danger] = 'ユーザ登録に失敗しました．'
+      render :new
+    end
+  end
+
   def edit
   end
+
+  def update
+  end
+
+  private
+
+    def user_params
+      param.fetch(:user, {}).permit(
+        :name,
+        :email,
+        :password,
+        :password_confirmation,
+      )
+    end
 end
