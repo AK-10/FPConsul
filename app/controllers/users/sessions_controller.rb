e30860c01505656bad8b6fd523510612a2d46515
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-25 17:16:17 +0900

    impl session.create

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,14 +1,17 @@
 class Users::SessionsController < ApplicationController
   def new
-
   end
 
   def create
-
+    user = User.find_by(session_params[:email])
+    if user && user.authenticate(session_params[:password])
+      redirect_to user
+    else
+      render :new, status: :bad_request
+    end
   end
 
   def destroy
-    
   end
 
   private
