52d2fb29080d626826396ee16239dadd97a75a63
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-25 15:55:11 +0900

    rubocop

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -2,7 +2,6 @@
 
 require "rails_helper"
 
-
 RSpec.describe UsersController, type: :request do
   describe "GET /users/join" do
     it { is_expected.to eq(200) }
@@ -13,8 +12,8 @@ RSpec.describe UsersController, type: :request do
       let(:params) {
         {
           user: {
-            name: 'test2',
-            email: 'qwert@test.com',
+            name: "test2",
+            email: "qwert@test.com",
             password: "qpwoei1029",
             password_confirmation: "qpwoei1029"
           }
@@ -27,8 +26,8 @@ RSpec.describe UsersController, type: :request do
       let(:params) {
         {
           user: {
-            name: 'test2',
-            email: 'qwertterws',
+            name: "test2",
+            email: "qwertterws",
             password: "qpwoei1029",
             password_confirmation: "qpwoei1029"
           }
@@ -37,5 +36,4 @@ RSpec.describe UsersController, type: :request do
       it { is_expected.to eq(400) }
     end
   end
-
 end
