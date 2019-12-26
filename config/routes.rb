06e4ec632c24f93b01a6dc4e05f2a6c3e8f0172c
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-26 11:12:25 +0900

    change sessions routing out of scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  scope(path_names: { new: "join" }) do
+  scope(path_names: { new: "join", show: 'home' }) do
     resource :user, only: %i(new show create edit update)
-    # もっと上手い書き方がありそう
-    resource :users do
-      get "/login", to: "users/session#new"
-      post "/login", to: "users/session#create"
-      delete "/logout", to: "users/session#destroy"
-    end
+  end
+
+  # もっと良い書き方がありそう
+  resource :user do
+    get "/login", to: "users/session#new"
+    post "/login", to: "users/session#create"
+    delete "/logout", to: "users/session#destroy"
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
