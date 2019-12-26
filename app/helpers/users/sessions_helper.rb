18c3c0d8445a6f7985a82a6d02c0ad092469a670
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-26 09:52:06 +0900

    impl current_user and logged_in_by_user?

--- app/helpers/users/sessions_helper.rb
+++ app/helpers/users/sessions_helper.rb
@@ -4,8 +4,17 @@ module Users
       session[:user_id] = user.id
     end
 
+    def current_user
+      @current_user ||= User.find_by(id: session[user_id])
+    end
+
+    def logged_in_by_user?
+      !current_user.nil?
+    end
+
     def logout_by_user
       session.delete(:user_id)
+      @current_user = nil
     end
   end
 end
