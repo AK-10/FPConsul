538f31105761b4f4e6806bb639cb4ebfba55a938
AK-10 <konishi.atsushi@moneyforward.co.jp>
2019-12-26 10:33:57 +0900

    change routing resources users -> resource user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   scope(path_names: { new: "join" }) do
-    resources :users, only: %i(new show create edit update)
+    resource :user, only: %i(new show create edit update)
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
