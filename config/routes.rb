eb2c776fddbf6f01b2398d683f2fdeb2eef34079
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-01-05 07:28:12 +0900

    fix selecting action for session

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
 
   # もっと良い書き方がありそう
   resource :user do
-    get "/login", to: "users/sessions#new"
-    post "/login", to: "users/sessions#create"
-    delete "/logout", to: "users/sessions#destroy"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
