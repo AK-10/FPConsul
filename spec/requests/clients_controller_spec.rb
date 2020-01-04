3c3aae69d52f86b74b5ced77f0ccba9216cba2e8
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-01-05 07:24:39 +0900

    fix routing for clients_request spec

--- spec/requests/clients_controller_spec.rb
+++ spec/requests/clients_controller_spec.rb
@@ -2,16 +2,16 @@
 
 require "rails_helper"
 
-RSpec.describe UsersController, type: :request do
-  describe "GET /user/join" do
+RSpec.describe ClientsController, type: :request do
+  describe "GET /clients/join" do
     it { is_expected.to eq(200) }
   end
 
-  describe "POST /user" do
+  describe "POST /clients" do
     context "valid params" do
       let(:params) {
         {
-          user: {
+          client: {
             name: "test2",
             email: "qwert@test.com",
             password: "qpwoei1029",
@@ -25,7 +25,7 @@ RSpec.describe UsersController, type: :request do
     context "invalid params" do
       let(:params) {
         {
-          user: {
+          client: {
             name: "test2",
             email: "qwertterws",
             password: "qpwoei1029",
