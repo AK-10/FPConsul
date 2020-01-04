a3bee0d1c939e58816cb576bb5fa060e7336871c
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-01-05 03:05:56 +0900

    add spec for user_type

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   }
 
   has_secure_password
-  has_many :reservations, dependent: :destroy
+  # has_many :reservations, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 100 }, format: { with: ValidFormat::EMAIL_FORMAT }
