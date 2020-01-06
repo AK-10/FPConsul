97fe7e99c9492f3c552947cfe11b6dca897565a9
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-06 11:06:28 +0900

    add planner_params

--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -1,2 +1,30 @@
 class PlannersController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @planner = Planner.new
+  end
+
+  def create
+    @planner = Planner.new(planner_params)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+    def planner_params
+      params.require(:planner).permit(
+        :name,
+        :email,
+        :password,
+        :password_confirmation,
+      )
+    end
+
 end
