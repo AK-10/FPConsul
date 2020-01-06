debe73a9702d31842810d185d5d295c1b63b96a5
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-06 11:45:35 +0900

    impl planners controller spec

--- spec/requests/planners_controller_spec.rb
+++ spec/requests/planners_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe PlannersController, type: :controller do
+RSpec.describe PlannersController, type: :request do
+  describe "GET /planners/join" do
+    it { is_expected.to eq(200) }
+  end
 
+  describe "POST /planners" do
+    context "valid params" do
+      let(:params) {
+        {
+          planner: {
+            name: "planner test2",
+            email: "qwert@planner.com",
+            password: "qpwoei1029",
+            password_confirmation: "qpwoei1029"
+          }
+        }
+      }
+      it { is_expected.to eq(302) }
+    end
+
+    context "invalid params" do
+      let(:params) {
+        {
+          planner: {
+            name: "test2",
+            email: "qwertterws",
+            password: "qpwoei1029",
+            password_confirmation: "qpwoei1029"
+          }
+        }
+      }
+      it { is_expected.to eq(422) }
+    end
+  end
 end
