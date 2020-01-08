5ce7a18e4f1e0a8ccbe9e05215f1b17bd5fb3044
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-08 14:35:36 +0900

    impl helper method time_ranges

--- app/helpers/available_frames_helper.rb
+++ app/helpers/available_frames_helper.rb
@@ -1,2 +1,18 @@
 module AvailableFramesHelper
+  def time_ranges
+    first_time = Tod::TimeOfDay.new 10
+    last_time = Tod::TimeOfDay.new 18
+    step_min = 30.minutes
+    scopes = []
+
+    while true do
+      end_time = first_time + step_min
+      break if end_time > last_time
+
+      scopes << "#{first_time.to_s} ~ #{end_time.to_s}"
+      first_time = end_time
+    end
+
+    scopes
+  end
 end
