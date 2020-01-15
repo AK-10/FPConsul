d96660803dec72fb0d5efb879ab076d02e8ec550
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-15 10:26:50 +0900

    add not null constraint to reservations.available_frame_id

--- db/migrate/20200114012154_add_available_frame_id_column_and_remove_planner_id_column_and_scheduled_time.rb
+++ db/migrate/20200114012154_add_available_frame_id_column_and_remove_planner_id_column_and_scheduled_time.rb
@@ -1,6 +1,6 @@
 class AddAvailableFrameIdColumnAndRemovePlannerIdColumnAndScheduledTime < ActiveRecord::Migration[5.2]
   def up
-    add_reference :reservations, :available_frame, foreign_key: true, index: { unique: true }
+    add_reference :reservations, :available_frame, null: false, foreign_key: true, index: { unique: true }
     remove_index :reservations, [:planner_id, :scheduled_time]
     remove_reference :reservations, :planner, foreign_key: { to_table: :users }, index: true
     remove_column :reservations, :scheduled_time, null: false
