2712801e0bd662e4c8c7bdd91b713b7e8242f942
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-15 10:43:07 +0900

    fix validation spec for resrvation, failed uniqueness validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,12 +5,6 @@ class Reservation < ApplicationRecord
   belongs_to :available_frame
 
   validates :client, presence: true
-  validates :available_frame, presence: true, unique: true
+  validates :available_frame, presence: true, uniqueness: true
   validates :client_id, uniqueness: { scope: [:available_frame_id] }
-
-  private
-    def validate_available_frame_exists
-      return if planner.available_frames.where(scheduled_time: scheduled_time).exists?
-      errors.add(:scheduled_time, "is unavailable")
-    end
 end
