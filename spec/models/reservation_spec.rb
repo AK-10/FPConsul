00bc37f5e99066756411e8e7f444c8e0ce55ccc4
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-16 11:22:05 +0900

    fix uniqueness validation for reservation: success

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -4,10 +4,20 @@ require "rails_helper"
 
 RSpec.describe Reservation, type: :model do
   describe "validations" do
+    
     it { is_expected.to validate_presence_of(:client) }
     it { is_expected.to validate_presence_of(:available_frame) }
-    it { is_expected.to validate_uniqueness_of(:available_frame) }
-    it { is_expected.to validate_uniqueness_of(:client_id).scoped_to(:available_frame_id) }
+    
+    context "uniqueness validation" do
+      around do |e|
+        travel_to("2019-12-18 12:00:00") { e.run }
+      end
+
+
+      include_context "reservation_have_already_created"
+      it { is_expected.to validate_uniqueness_of(:available_frame) }
+      it { is_expected.to validate_uniqueness_of(:client_id).scoped_to(:available_frame_id) }
+    end
   end
 
   describe "associations" do
--- spec/support/shared_contexts/reservation.rb
+++ spec/support/shared_contexts/reservation.rb
@@ -1,12 +1,8 @@
 
 RSpec.shared_context "reservation_have_already_created" do
+  let(:available_frame) { create(:available_frame, scheduled_time: "2019-12-19 12:00:00") }
 
   before do
-    around do |e|
-      travel_to("2019-12-18 12:00:00") { e.run }
-    end
-
-    create(:avilable_frame, scheduled_time: "2019-12-19 12:00:00")
     create(:reservation, available_frame: available_frame)
   end
 end
