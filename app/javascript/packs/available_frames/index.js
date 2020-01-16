494a9ee063780ab4d62bbbf7b8b6c87d6962653e
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-16 16:28:42 +0900

    WIP: fix destroy frame modal and index.js

--- app/javascript/packs/available_frames/index.js
+++ app/javascript/packs/available_frames/index.js
@@ -1,23 +1,31 @@
-const setModalContent = (isAvailable, datetime) => {
-    let paragTag = document.getElementById('available_frame-request-confirm-text')
+const setModalContent = (available, datetime) => {
+    const idName = undefined !== avilalble ? 'inactivate-frame-confirm-text' : 'activate-frame-confirm-text'
+    let paragTag = document.getElementById(idName)
+    console.log(paragTag)
 
-    const performText = !isAvailable ? "有効" : "無効"
+    const performText = !available ? "有効" : "無効"
+
     paragTag.innerText = `${datetime}の予約枠を${performText}にしますか?`
 }
 
-const setDatetimeToTextField = (datetime) => {
+const setDatetimeToTextField = datetime => {
      let textField = document.getElementById("available_frame-scheduled_time")
      textField.value = datetime
 }
+
+// const setDestroyHref = () => {
+//     setHref =
+// }
 
 window.onload = () => {
     const triggerButtons = document.getElementsByClassName('available_frame-trigger-btn')
 
     Array.from(triggerButtons).forEach( btn => {
         btn.onclick = () => {
-            const isAvailable = btn.dataset.is_available === "true"
+            console.log(btn.dataset)
+            const available = btn.dataset.path !== undefined
             const datetime = btn.dataset.datetime
-            setModalContent(isAvailable, datetime)
+            setModalContent(available, datetime)
             setDatetimeToTextField(datetime)
         }
     })
