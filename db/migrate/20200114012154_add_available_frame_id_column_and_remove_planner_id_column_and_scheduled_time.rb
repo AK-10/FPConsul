ae712fcfe7cbb545b30e81341712fdc44892eab4
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-20 11:44:30 +0900

    remove multiple unique index [client_id, available_frame_id]

--- db/migrate/20200114012154_add_available_frame_id_column_and_remove_planner_id_column_and_scheduled_time.rb
+++ db/migrate/20200114012154_add_available_frame_id_column_and_remove_planner_id_column_and_scheduled_time.rb
@@ -4,14 +4,12 @@ class AddAvailableFrameIdColumnAndRemovePlannerIdColumnAndScheduledTime < Active
     remove_index :reservations, [:planner_id, :scheduled_time]
     remove_reference :reservations, :planner, foreign_key: { to_table: :users }, index: true
     remove_column :reservations, :scheduled_time, null: false
-
-    add_index :reservations, [:client_id, :available_frame_id], unique: true
   end
 
   def down
     add_reference :reservations, :planner, foreign_key: { to_table: :users }
     remove_reference :reservations, :available_frame, foreign_key: true, index: true
-  
+
     add_column :reservations, :scheduled_time, :datetime, null: false, comment: "予約枠の日時(開始時間)"
     add_index :reservations, [:planner_id, :scheduled_time], unique: true
   end
