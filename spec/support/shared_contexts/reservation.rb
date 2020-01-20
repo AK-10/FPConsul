4702880e387b7befefa336d36590bdbf5ce81dec
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-20 18:13:45 +0900

    replace do block -> brace block

--- spec/support/shared_contexts/reservation.rb
+++ spec/support/shared_contexts/reservation.rb
@@ -1,7 +1,5 @@
 RSpec.shared_context "reservation_have_already_created" do
   let(:available_frame) { create(:available_frame, scheduled_time: "2019-12-19 12:00:00") }
 
-  before do
-    create(:reservation, available_frame: available_frame)
-  end
+  before { create(:reservation, available_frame: available_frame) }
 end
