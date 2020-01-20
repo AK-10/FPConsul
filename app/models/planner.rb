4db35ec0c3884a4d9e67d01f95410226b8f0378f
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-20 16:44:27 +0900

    remove comment for planner through available_frame

--- app/models/planner.rb
+++ app/models/planner.rb
@@ -4,6 +4,5 @@ class Planner < User
   default_scope { where(user_type: :planner) }
 
   has_many :available_frames, dependent: :destroy
-  # planner.reservations == planner.available_frames.joins(:reservations)
   has_many :reservations, dependent: :destroy, through: :available_frames
 end
