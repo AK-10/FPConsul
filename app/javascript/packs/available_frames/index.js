faa34ed0431f4b83784908983130bd027e5e2806
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-20 10:25:22 +0900

    yarn add moment

--- app/javascript/packs/available_frames/index.js
+++ app/javascript/packs/available_frames/index.js
@@ -1,16 +1,14 @@
 const setModalContent = (available, datetime) => {
     const idName = available ? 'inactivate-frame-confirm-text' : 'activate-frame-confirm-text'
     let paragTag = document.getElementById(idName)
-    console.log(paragTag)
 
     const performText = !available ? "有効" : "無効"
-
     paragTag.innerText = `${datetime}の予約枠を${performText}にしますか?`
 }
 
 const setDatetimeToTextField = datetime => {
-     let textField = document.getElementById("available_frame-scheduled_time")
-     textField.value = datetime
+    let textField = document.getElementById("available_frame-scheduled_time")
+    textField.value = datetime
 }
 
 const setDestroyHref = (path) => {
