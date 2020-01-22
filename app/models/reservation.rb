e7525a3034472bd9a9feb3918f3239f7ee8b652f
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-22 11:25:55 +0900

    isolate find_by_scheduled_time

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,16 +4,18 @@ class Reservation < ApplicationRecord
   belongs_to :client
   belongs_to :available_frame
 
+  scope :find_by_scheduled_time, -> (time) do
+    return if time.nil?
+    joins(:available_frame).where(available_frames: { scheduled_time: time })
+  end
+
   validates :client, presence: true
   validates :available_frame, presence: true, uniqueness: true
   validate :other_reservation_exists_in_same_time, if: [:client, :available_frame]
 
   private
     def other_reservation_exists_in_same_time
-      presence = client.reservations.joins(:available_frame)
-        .where(available_frames: { scheduled_time: available_frame.scheduled_time })
-        .exists?
-
+      presence = client.reservations.find_by_scheduled_time(available_frame.scheduled_time).exists?
       errors.add(:available_frame, "scheduled_time already exists") if presence
     end
 end
