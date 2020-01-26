16973bb6027596311be1bb806ecf53445e9e5f11
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-01-26 23:01:10 +0900

    impl spec for clients/reservations create action

--- spec/requests/clients/reservations_controller_spec.rb
+++ spec/requests/clients/reservations_controller_spec.rb
@@ -28,4 +28,69 @@ RSpec.describe Clients::AvailableFramesController, type: :request do
       end
     end
   end
+
+  describe "POST /clients/:client_id/reservations" do
+    context "available_frame exist" do
+      let(:planner) { create(:planner) }
+      let(:available_frame) { planner.available_frames.create(scheduled_time: "2019-12-18 13:00:00") }
+
+      let(:params) {
+        {
+          reservation: {
+            available_frame_id: available_frame.id
+          }
+        }
+      }
+
+      it do
+        is_expected.to redirect_to client_available_frames_path(client)
+        expect(flash[:success]).to eq(["予約しました(#{available_frame.scheduled_time.strftime("%Y年 %m月 %d日 (%a) %T")})"])
+      end
+    end
+
+    context "available_frame not exist" do
+      let(:available_frame_id) do
+        AvailableFrame.last&.id || 1
+      end
+      let(:params) {
+        {
+          reservation: {
+            available_frame_id: available_frame_id
+          }
+        }
+      }
+
+      it do
+        is_expected.to redirect_to client_available_frames_path(client)
+        expect(flash[:danger]).to eq(["Available frame must exist", "Available frame can't be blank"])
+      end
+    end
+
+    context "other reservation exists in same time" do
+      # 同じ時間のavailable_frameを2つ作る
+      # 一つのclientで二つとも予約しようとする
+      before do
+        create(:planner) do |plnr|
+          af = create(:available_frame, planner: plnr, scheduled_time: "2019-12-18 13:00:00")
+          create(:reservation, available_frame: af, client: client)
+        end
+      end
+
+      let(:planner) { create(:planner) }
+      let(:available_frame) { planner.available_frames.create(scheduled_time: "2019-12-18 13:00:00") }
+
+      let(:params) {
+        {
+          reservation: {
+            available_frame_id: available_frame.id
+          }
+        }
+      }
+
+      it do
+        is_expected.to redirect_to client_available_frames_path(client)
+        expect(flash[:danger]).to eq(["Available frame scheduled_time already exists"])
+      end
+    end
+  end
 end
