3ea618d4ff779732d2b114966fd7c21432637beb
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-01-26 18:42:24 +0900

    impl reservation.create action

--- app/controllers/clients/reservations_controller.rb
+++ app/controllers/clients/reservations_controller.rb
@@ -9,4 +9,26 @@ class Clients::ReservationsController < ApplicationController
     end
     @available_frames = AvailableFrame.not_reserved.where(scheduled_time: datetime)
   end
+
+  def create
+    reservation = current_client.reservations.build(reservation_param)
+    if reservation.save
+      time = reservation.available_frame.scheduled_time
+      flash[:success] = ["予約しました(#{time.strftime("%Y年 %m月 %d日 (%a) %T")})"]
+    else
+      flash[:danger] = reservation.errors.full_messages
+    end
+
+    redirect_to client_available_frames_path(current_client)
+  end
+
+  def destroy
+  end
+
+  private
+    def reservation_params
+      params.require(:reservation).permit(
+        :available_frame_id
+      )
+    end
 end
