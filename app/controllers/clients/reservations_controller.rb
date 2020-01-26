a9afb5d0d34e0683f17f8a2ba05a37b3079e8268
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-01-26 18:44:56 +0900

    fix absence path frase

--- app/controllers/clients/reservations_controller.rb
+++ app/controllers/clients/reservations_controller.rb
@@ -1,11 +1,11 @@
 class Clients::ReservationsController < ApplicationController
   before_action :logged_in_as_client?
-  
+
   def new
     datetime = params[:datetime]
     unless datetime
       flash[:danger] = ["時間が指定されていません"]
-      redirect_to client_available_frames(current_client)
+      redirect_to client_available_frames_path(current_client)
     end
     @available_frames = AvailableFrame.not_reserved.where(scheduled_time: datetime)
   end
