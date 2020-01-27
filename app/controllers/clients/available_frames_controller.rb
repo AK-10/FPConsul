82b3bdc14891542e28fd5aea065b33b539d1c02d
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-27 16:39:08 +0900

    fix strange names, rescue Error on params[:from].in_time_zone

--- app/controllers/clients/available_frames_controller.rb
+++ app/controllers/clients/available_frames_controller.rb
@@ -4,8 +4,11 @@ class Clients::AvailableFramesController < ApplicationController
   before_action :logged_in_as_client?
 
   def index
-    @from = (params[:from]&.in_time_zone || Time.current).change(hour: 0, min: 0, sec: 0)
-    @to = @from.since(7.days)
-    @available_frames = AvailableFrame.not_reserved.where(scheduled_time: (@from)..(@to))
+    from_time = (params[:from]&.in_time_zone || Time.current) rescue Time.current
+
+    @scheduled_time_from = from_time.begginng_of_day
+    scheduled_time_to = @scheduled_time_from.since(7.days)
+
+    @available_frames = AvailableFrame.not_reserved.where(scheduled_time: (@scheduled_time_from)..(scheduled_time_to))
   end
 end
