c0ac7c577d453b19a0fe3e89defc4201619f7553
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-27 16:50:36 +0900

    fix start_time, replace @from -> @scheduled_time_from on index.html.slim

--- app/controllers/clients/available_frames_controller.rb
+++ app/controllers/clients/available_frames_controller.rb
@@ -4,9 +4,9 @@ class Clients::AvailableFramesController < ApplicationController
   before_action :logged_in_as_client?
 
   def index
-    from_time = (params[:from]&.in_time_zone || Time.current) rescue Time.current
+    start_time = (params[:from]&.in_time_zone || Time.current) rescue Time.current
 
-    @scheduled_time_from = from_time.begginng_of_day
+    @scheduled_time_from = start_time.beginning_of_day
     scheduled_time_to = @scheduled_time_from.since(7.days)
 
     @available_frames = AvailableFrame.not_reserved.where(scheduled_time: (@scheduled_time_from)..(scheduled_time_to))
