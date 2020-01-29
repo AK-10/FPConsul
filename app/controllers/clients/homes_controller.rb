c425ef767087ff0a8a21041224ae73edc2782256
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-29 14:55:06 +0900

    change outer join to inner join at read reservation

--- app/controllers/clients/homes_controller.rb
+++ app/controllers/clients/homes_controller.rb
@@ -4,6 +4,8 @@ class Clients::HomesController < ApplicationController
   before_action :logged_in_as_client?
 
   def show
-    @reservations = current_client.reservations.eager_load(available_frame: :planner)
+    @reservations = current_client.reservations
+                      .joins(available_frame: :planner)
+                      .eager_load(available_frame: :planner)
   end
 end
--- app/controllers/planners/homes_controller.rb
+++ app/controllers/planners/homes_controller.rb
@@ -5,5 +5,7 @@ class Planners::HomesController < ApplicationController
 
   def show
     @reservations = current_planner.reservations
+                      .joins([:available_frame, :client])
+                      .eager_load([:available_frame, :client])
   end
 end
