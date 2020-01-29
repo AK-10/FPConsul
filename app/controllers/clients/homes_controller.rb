ae730a3d6b8db0154a4960043bce8878a391bea1
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-29 10:36:38 +0900

    outer join available_frame and planner

--- app/controllers/clients/homes_controller.rb
+++ app/controllers/clients/homes_controller.rb
@@ -4,6 +4,6 @@ class Clients::HomesController < ApplicationController
   before_action :logged_in_as_client?
 
   def show
-    @reservations = current_client.reservations.eager_load(:available_frame)
+    @reservations = current_client.reservations.eager_load(available_frame: :planner)
   end
 end
