d72bed7b708912886b7ad4c04cfe55e970eac76d
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-30 11:29:51 +0900

    impl available_frame_decorator_spec

--- spec/decorators/available_frame_decorator_spec.rb
+++ spec/decorators/available_frame_decorator_spec.rb
@@ -3,7 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe AvailableFrameDecorator do
-  let(:available_frame) { AvailableFrame.new.extend AvailableFrameDecorator }
-  subject { available_frame }
-  it { should be_a AvailableFrame }
+  include_context "travel_to_20191218_noon"
+
+  subject { available_frame.formatted_scheduled_time }
+  context "future, not appearing (終了)" do
+    let(:available_frame) { build(:available_frame, scheduled_time: "2019-12-18 12:30:00") }
+    it { is_expected.to eq("2019年 12月 18日 (Wed) 12:30 ~ 13:00") }
+  end
+
+  context "past, appearing (終了)" do
+    let(:available_frame) { build(:available_frame, scheduled_time: "2019-12-17 12:30:00") }
+    it { is_expected.to eq("2019年 12月 17日 (Wed) 12:30 ~ 13:00(終了)") }
+  end
+
 end
