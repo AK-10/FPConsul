e71b5f1cca18341c178b955f3a68e96bd2750628
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-01-30 13:01:35 +0900

    refactor AvailableFrame.formatted_scheduled_time

--- app/decorators/available_frame_decorator.rb
+++ app/decorators/available_frame_decorator.rb
@@ -2,18 +2,10 @@
 
 module AvailableFrameDecorator
   def formatted_scheduled_time
-    # start_time = scheduled_time.strftime("%H:%M")
-    # end_time = (scheduled_time + 30.minutes).strftime("%H:%M") # 略しすぎ？
-    # scheduled_date = scheduled_time.strftime("%Y年 %m月 %d日 (%a)")
-
-    start_time = scheduled_time
-    end_time = scheduled_time + 30.minutes
-
-    scheduled_date_format_string = scheduled_time.strftime("%Y年 %m月 %d日 (%a)")
-    start_time_format_string = start_time.strftime("%H:%M")
-    end_time_format_string = end_time.strftime("%H:%M")
-
+    start_time, end_time = [scheduled_time, scheduled_time + 30.minutes].map { |time| time.strftime("%H:%M") }
+    scheduled_date = scheduled_time.strftime("%Y年 %m月 %d日 (%a)")
     finished = scheduled_time.past? ? "(終了)" : ""
-    "#{scheduled_date_format_string} #{start_time_format_string} ~ #{end_time_format_string}#{finished}"
+
+    "#{scheduled_date} #{start_time} ~ #{end_time}#{finished}"
   end
 end
