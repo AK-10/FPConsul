4bc9aadb868dbd1a31cf28d079dafe950939cabd
AK-10 <konishi.atsushi@moneyforward.co.jp>
2020-02-03 15:08:18 +0900

    fix concern method definition

--- app/controllers/concerns/client_concern.rb
+++ app/controllers/concerns/client_concern.rb
@@ -6,21 +6,21 @@ module ClientConcern
 
   included do
     helper_method :current_client
+  end
 
-    def current_client
-      current_user.becomes(Client) if current_user.client?
-    end
+  def current_client
+    current_user.becomes(Client) if current_user.client?
+  end
 
-    def logged_in_as_client?
-      !!current_client
-    end
+  def logged_in_as_client?
+    !!current_client
+  end
 
-    def require_client_login!
-      require_login!
-      unless logged_in_as_client?
-        flash[:warning] = "クライアントユーザ以外のアクセスは許可されていません"
-        redirect_to current_user.home_path
-      end
+  def require_client_login!
+    require_login!
+    unless logged_in_as_client?
+      flash[:warning] = "クライアントユーザ以外のアクセスは許可されていません"
+      redirect_to current_user.home_path
     end
   end
 end
--- app/controllers/concerns/session_concern.rb
+++ app/controllers/concerns/session_concern.rb
@@ -5,30 +5,30 @@ module SessionConcern
 
   included do
     helper_method :current_user, :logged_in?
+  end
 
-    def current_user
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 
-    def login(user)
-      session[:user_id] = user.id
-    end
+  def login(user)
+    session[:user_id] = user.id
+  end
 
-    def logged_in?
-      !!current_user
-    end
+  def logged_in?
+    !!current_user
+  end
 
-    def require_login!
-      unless logged_in?
-        flash[:warning] = "ログインしてください"
-        redirect_to "/user/login"
-      end
+  def require_login!
+    unless logged_in?
+      flash[:warning] = "ログインしてください"
+      redirect_to "/user/login"
     end
+  end
 
-    def redirect_to_home
-      # インスタンスを引数に渡しても期待するpathが得られない
-      # (/(clients|planners)/:id にならない)
-      redirect_to current_user.home_path if logged_in?
-    end
+  def redirect_to_home
+    # インスタンスを引数に渡しても期待するpathが得られない
+    # (/(clients|planners)/:id にならない)
+    redirect_to current_user.home_path if logged_in?
   end
 end
